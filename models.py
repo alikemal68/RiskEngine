cd874013902bef1119ec53463c893ba25a42c01f
Ali Kemal Özkaya <alikemal68@hotmail.de>
2026-09-13 12:55:50 +0200

    worked on EWMA covar estimator

--- models.py
+++ models.py
@@ -1,25 +1,45 @@
 import numpy as np, pandas as pd
 
-def EWMA_covar_estimator(risk_factor, theta=0.04, burn_in_window=100):
+
+def EWMA_covar_estimator(
+        risk_factor,
+        initial_covar, 
+        decay=0.96, 
+        initialization_window=60, 
+        burn_in_window=30):
 
     T,N = risk_factor.shape
 
-    covar = np.full((T, N*(N+1)/2), np.nan)
+    covariances = np.full((T, N, N), np.nan)
 
-    # Initial covariance estimate
-    covar[burn_in_window] = risk_factor.iloc[:burn_in_window].cov()
-#     Sigma_1 is usually set as the
-# unconditional volatility of the data and some 30 days of data are used to update the
-# volatility forecast before it is used
-# !!!!!!!!!!!!!!!!!!!!.
+    if initial_covar is None:
+        sigma0 = (
+                risk_factor
+                .iloc[:initialization_window]
+                .cov()
+                .to_numpy()
+            )
+
+        # Sigma_0 is the forecast for the first observation AFTER the initialization sample.
+        start = initialization_window   
+
+    else:
+        sigma0 = initial_covar
+        start = 0 
+     
+
+    # Initial covariancesiance estimate
+
+    covariances[start] = sigma0 #risk_factor.iloc[:start].cov()
 
     # EWMA recursion
-    for t in range(burn_in_window + 1, T):
+    for t in range(start + 1, T):
         x = risk_factor.iloc[t - 1]
 
-        covar[t] = (
-            theta * np.outer(x, x)
-            + (1 - theta) * covar[t - 1]
+        covariances[t] = (
+            (1-decay) * np.outer(x, x) + decay * covariances[t - 1]
         )
 
-    return covar
+    covariances[start:start+burn_in_window] = np.nan
+
+    return covariances
